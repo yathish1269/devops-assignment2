8d08cdaa448ea187c24bc99d8557a94653c6b71a
yathishkumar <yathishkumar1269@gmail.com>
2019-08-19 14:59:47 -0400

    Created Java project for assignment 2

---
+++ src/main/java/SampleAssignment2/Assignment2/App.java
@@ -0,0 +1,13 @@
+package SampleAssignment2.Assignment2;
+
+/**
+ * Main class for Assignment2
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Main class for Assignment2" );
+    }
+}
