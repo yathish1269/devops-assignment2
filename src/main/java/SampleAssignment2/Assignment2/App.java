210532d578c692f1df38b58de80951bcea548a28
yathish1269 <52585117+yathish1269@users.noreply.github.com>
2019-08-20 09:54:51 -0400

    Update App.java

--- src/main/java/SampleAssignment2/Assignment2/App.java
+++ src/main/java/SampleAssignment2/Assignment2/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Main class for Assignment2" );
+        System.out.println(" Main class for Assignment2 ");
     }
 }
